41167b03bf1aabece8cefb00520a57f940a90d51
yachiK <yachi1520151@gmail.com>
2017-11-20 23:51:39 +0900

    aaa

---
+++ src/main/java/com/example/sokisoba/SokisobaApplication.java
@@ -0,0 +1,12 @@
+package com.example.sokisoba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SokisobaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SokisobaApplication.class, args);
+	}
+}
